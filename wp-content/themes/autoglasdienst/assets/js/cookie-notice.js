4579123b3c7f7f2b657086d5e1f0ce4223a0270d
media-admin <markus.tritremmel@media-lab.at>
2022-05-02 14:50:01 +0200

    Fixed minor issues

--- wp-content/themes/autoglasdienst/assets/js/cookie-notice.js
+++ wp-content/themes/autoglasdienst/assets/js/cookie-notice.js
@@ -1,94 +1,259 @@
 <script>
 
-  document.addEventListener("DOMContentLoaded", function() {
+	document.addEventListener("DOMContentLoaded", function() {
 
-  dywc.init({
+		dywc.init({
 
-   cookie_version: 1, // Version der Cookiedefinition, damit bei Konfigurationsänderung erneutes Opt-In erforderlich wird
-   cookie_name: 'dywc', // Name des Cookies, der zur Speicherung der Entscheidung verwendet wird
-   cookie_expire: 31536e3, // Laufzeit des Cookies in Sekunden (31536e3 = 1Jahr)
-   cookie_path: '/', // Pfad auf dem der Cookie gespeichert wird
-   mode: 1, // 1 oder 2, bestimmt den Buttonstil 
-   bglayer: true, // Verdunklung des Hintegrundes aktiv (true) oder inaktiv (false)
-   position: 'mt', // mt, mm, mb, lt, lm, lb, rt, rm, rb
+		 cookie_version: 1, // Version der Cookiedefinition, damit bei Konfigurationsänderung erneutes Opt-In erforderlich wird
+		 cookie_name: 'dywc', // Name des Cookies, der zur Speicherung der Entscheidung verwendet wird
+		 cookie_expire: 31536e3, // Laufzeit des Cookies in Sekunden (31536e3 = 1Jahr)
+		 cookie_path: '/', // Pfad auf dem der Cookie gespeichert wird
+		 mode: 1, // 1 oder 2, bestimmt den Buttonstil
+		 bglayer: true, // Verdunklung des Hintegrundes aktiv (true) oder inaktiv (false)
+		 position: 'mt', // mt, mm, mb, lt, lm, lb, rt, rm, rb
 
-   id_bglayer: 'dywc_bglayer',
-   id_cookielayer: 'dywc',
-   id_cookieinfo: 'dywc_info',
+		 id_bglayer: 'dywc_bglayer',
+		 id_cookielayer: 'dywc',
+		 id_cookieinfo: 'dywc_info',
 
-   url_legalnotice: '/datenschutzerklaerung.html', // or null
-   url_imprint: '/impressum.html', // or null
+		 url_legalnotice: '/datenschutzerklaerung', // or null
+		 url_imprint: '/impressum', // or null
 
-   text_title: 'Datenschutzeinstellungen',
-   text_dialog: 'Wir nutzen Cookies auf unserer Website. Einige von ihnen sind essenziell, während andere uns helfen, diese Website und Ihre Erfahrung zu verbessern.',
+		 text_title: 'Datenschutzeinstellungen',
+		 text_dialog: 'Wir nutzen Cookies auf unserer Website. Einige von ihnen sind essenziell, während andere uns helfen, diese Website und Ihre Erfahrung zu verbessern.',
 
-   cookie_groups: [
-	{
-	 label: 'Notwendig',
-	 fixed: true,
-	 info: 'Zum Betrieb der Seite notwendige Cookies:',
-	 cookies: [
-	  {
-	   label: 'PHP Session Cookie',
-	   publisher: 'Eigentümer dieser Website',
-	   aim: 'Absicherung Kontaktformular / SPAM Schutz',
-	   name: 'PHPSESSID',
-	   duraction: 'Session'
-	  }, {
-	   label: 'Cookiespeicherung Entscheidungscookie',
-	   publisher: 'Eigentümer dieser Website',
-	   aim: 'Speichert die Einstellungen der Besucher bezüglich der Speicherung von Cookies.',
-	   name: 'dywc',
-	   duration: '1 Jahr'
-	  }
-	 ]
-	}, {
-	 label: 'Statistiken',
-	 fixed: false,
-	 info: 'Cookies die zur Auswertung des Benutzerverhaltens notwendig sind:',
-	 cookies: [
-	  {
-	   label: 'Google Analytics',
-	   publisher: 'Google LLC',
-	   aim: 'Cookie von Google für Website-Analysen. Erzeugt statistische Daten darüber, wie der Besucher die Website nutzt.',
-	   name: '_ga,_gid',
-	   duration: '2 Jahre'
-	  }
-	 ],
-	 accept: function() {
+		 cookie_groups: [
+		{
+		 label: 'Notwendig',
+		 fixed: true,
+		 info: 'Zum Betrieb der Seite notwendige Cookies:',
+		 cookies: [
+			{
+			 label: 'PHP Session Cookie',
+			 publisher: 'Eigentümer dieser Website',
+			 aim: 'Absicherung Kontaktformular / SPAM Schutz',
+			 name: 'PHPSESSID',
+			 duraction: 'Session'
+			}, {
+			 label: 'Cookiespeicherung Entscheidungscookie',
+			 publisher: 'Eigentümer dieser Website',
+			 aim: 'Speichert die Einstellungen der Besucher bezüglich der Speicherung von Cookies.',
+			 name: 'dywc',
+			 duration: '1 Jahr'
+			}
+		 ]
+		}, {
+			label: 'Erleichterte Bedienung',
+			fixed: false,
+			info: 'Cookies zur Erleichterung der Bedienung für den Benutzer:',
+			cookies: [
+					{
+						label: 'Google Maps',
+						publisher: 'Google LLC',
+						aim: 'Cookie von Google für die Nutzung von Google Maps.',
+						name: 'NID',
+						duration: '6 Monate'
+					 }
+				],
 
-	  // Hier kommt der Opt-In Code für die jeweilige Cookie Gruppe rein
-	  // Folgendes Beispiel für Google Analytics
+					accept: function() {
 
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+						dywc.log("Load Statistic Tracking");
 
-	  ga('create', '{TrackingID}', 'auto');
-	  ga('send', 'pageview');
+						/* Document Ready Script */
+						document.ready = function( callback ) {
 
-	 },
-	 reject: function() {
+							if( document.readyState != 'loading' ) {
+								callback();
+							}
+							else {
+								document.addEventListener( 'DOMContentLoaded', callback );
+							}
 
-	  // Hier kommt der Opt-Out Code rein
-	  // Folgendes Beispiel für Google Analtics
+						};
 
-	  var disableStr = 'ga-disable-{TrackingID}';
+						/* Automatically resize the iFrame */
+						var iFrame2C = {};
+						iFrame2C.rescale = function( iframe, format ) {
 
-	  window[disableStr] = true; document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+							let formatWidth = parseInt( format.split(':')[0] );
+							let formatHeight = parseInt( format.split(':')[1] );
+							let formatRatio = formatHeight / formatWidth;
+							var iframeBounds = iframe.getBoundingClientRect();
 
-	  dywc.cookie.removeItem('_ga', '/', '.domain.de');
-	  dywc.cookie.removeItem('_gid', '/', '.domain.de');
-	  dywc.cookie.removeItem('_gat', '/', '.domain.de');
-	  dywc.cookie.removeItem('_gat_gtag_{TrackingID}', '/', '.domain.de');
+							let currentWidth = iframeBounds.width;
+							let newHeight = formatRatio * currentWidth;
 
-	 }
-	}
-   ]
+							iframe.style.height = Math.round( newHeight ) + "px";
+						};
 
-  });
+						/* Resize iFrame */
+						function iframeResize() {
 
-  });
-  
+							var iframes = document.querySelectorAll( 'iframe[data-scaling="true"]' );
+							if( !!iframes.length ) {
+								for( var i=0; i < iframes.length; i++ ) {
+									let iframe = iframes[ i ];
+									let videoFormat = '16:9';
+									let is_data_format_existing = typeof iframe.getAttribute( 'data-format' ) !== "undefined";
+									if( is_data_format_existing ) {
+										let is_data_format_valid = iframe.getAttribute( 'data-format' ).includes( ':' );
+										if( is_data_format_valid ) {
+											videoFormat = iframe.getAttribute( 'data-format' );
+										}
+									}
+									iFrame2C.rescale( iframe, videoFormat );
+								}
+							}
+						}
+
+						/* Event Listener on Resize for iFrame-Resizing */
+						document.ready( function() {
+							window.addEventListener( "resize", function() {
+								iframeResize();
+							});
+							iframeResize();
+						});
+
+						/* Source-URLs */
+						/*
+						 data_type will be the value of the attribute "data-type" on element "video_trigger"
+						 data_souce will be the value of the attribute "data-source" on element "video_trigger", which will be replaced on "{SOURCE}"
+						*/
+						function get_source_url( data_type ) {
+
+							switch( data_type ) {
+
+								case "google-maps":
+									return "https://www.google.com/maps/embed?pb={SOURCE}";
+								default: break;
+							}
+						}
+
+						/* 2-Click Solution */
+						document.ready( function() {
+
+							var video_wrapper = document.querySelectorAll( '.video_wrapper' );
+
+							if( !!video_wrapper.length ) {
+								for( var i=0; i < video_wrapper.length; i++ ) {
+									let _wrapper = video_wrapper[ i ];
+
+									var video_triggers = _wrapper.querySelectorAll( '.video_trigger' );
+									if( !!video_triggers.length ) {
+
+										for( var l=0; l < video_triggers.length; l++ ) {
+
+											var video_trigger = video_triggers[ l ];
+											var accept_buttons = video_trigger.querySelectorAll( 'input[type="button"]' );
+
+											if( !!accept_buttons.length ) {
+												for( var j=0; j < accept_buttons.length; j++ ) {
+
+													var accept_button = accept_buttons[ j ];
+													accept_button.addEventListener( "click", function() {
+
+														var _trigger = this.parentElement;
+														var data_type = _trigger.getAttribute( "data-type" );
+														var source = "";
+														_trigger.style.display = "none";
+
+														source = get_source_url( data_type );
+
+														var data_source = _trigger.getAttribute( 'data-source' );
+														source = source.replace( "{SOURCE}", data_source );
+
+														var video_layers = _trigger.parentElement.querySelectorAll( ".video_layer" );
+														if( !!video_layers.length ) {
+															for( var k=0; k < video_layers.length; k++ ) {
+
+																var video_layer = video_layers[ k ];
+																video_layer.style.display = "block";
+																video_layer.querySelector( "iframe" ).setAttribute( "src", source );
+
+															}
+														}
+
+														_wrapper.style.backgroundImage = "";
+														_wrapper.style.height = "auto";
+
+														var timeout = 100; // ms
+														setTimeout( function() {
+															iframeResize();
+														}, timeout );
+													});
+												}
+											}
+										}
+									}
+								};
+							}
+						});
+					},
+
+				reject: function() {
+
+					dywc.log("Reject Statistic Tracking");
+
+				}
+
+		}, {
+
+		 label: 'Statistiken',
+		 fixed: false,
+		 info: 'Cookies die zur Auswertung des Benutzerverhaltens notwendig sind:',
+		 cookies: [
+			{
+			 label: 'Google Analytics',
+			 publisher: 'Google LLC',
+			 aim: 'Cookie von Google für Website-Analysen. Erzeugt statistische Daten darüber, wie der Besucher die Website nutzt.',
+			 name: '_ga,_gid',
+			 duration: '2 Jahre'
+			}
+		 ],
+		 accept: function() {
+
+			dywc.log("Load Statistic Tracking");
+
+			var el = document.createElement('script');
+			el.src = 'https://www.googletagmanager.com/gtag/js?id=G-K1MSE6Z6X6';
+			el.async = 1;
+			document.getElementsByTagName('head')[0].appendChild(el);
+
+			window.dataLayer = window.dataLayer || [];
+
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-189126009-1', { 'anonymize_ip': true });
+			gtag('config', 'G-K1MSE6Z6X6', { 'anonymize_ip': true });
+
+		},
+
+		 reject: function() {
+
+			dywc.log("Reject Statistic Tracking");
+
+			var el = document.createElement('script');
+			el.src = 'https://www.googletagmanager.com/gtag/js?id=G-K1MSE6Z6X6';
+			el.async = 1;
+			document.getElementsByTagName('head')[0].appendChild(el);
+
+			window['ga-disable-G-K1MSE6Z6X6'] = true;
+			window.dataLayer = window.dataLayer || [];
+
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-189126009-1', { 'anonymize_ip': true });
+			gtag('config', 'G-K1MSE6Z6X6', { 'anonymize_ip': true });
+
+		 }
+		}
+		 ]
+
+		});
+
+	});
+
 </script>
--- wp-content/themes/autoglasdienst/functions.php
+++ wp-content/themes/autoglasdienst/functions.php
@@ -2,43 +2,106 @@
 /**
  * Theme Funktionen und allgemeine Definitionen für die Website "autoglasdienst.at"
  */
- 
-
 
 /* Allgemeine Theme Funktionen */
 
 function autoglasdienst_features() {
-	add_theme_support('title-tag');
-	add_theme_support('post-thumbnails');
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-formats', array( 'gallery' ) );
 }
 
 add_action('initafter_setup_theme', 'autoglasdienst_features');
 
 
+/* Beitragsbild aktivieren  */
 
-/* Aktivierung des Excerpts für statische Seiten */
+if ( ! function_exists( 'theme_slug_setup' ) ) :
+	function theme_slug_setup() {
+		add_theme_support( 'post-thumbnails' );
+	}
+endif;
 
-add_action( 'init', 'kb_page_excerpts' );
+add_action( 'after_setup_theme', 'theme_slug_setup' );
+
+
+/* Custom Background  */
+add_theme_support( 'custom-background' );
+
+
+/* Activate the Excerpt on Pages */
 
 function kb_page_excerpts() {
   add_post_type_support( 'page', 'excerpt' );
 }
 
+add_action( 'init', 'kb_page_excerpts' );
 
 
+/* Styles and Scripts */
 
+function autoglasdienst_register_styles() {
 
-/* Font Awesome importieren */
+	// Import FontAwesome
+	wp_register_style( 'fontawesome', get_template_directory_uri() . '/vendor/fontawesome-free-5.14.0-web/css/all.min.css' );
+	wp_enqueue_style( 'fontawesome' );
 
-function autoglasdienst_enqueue_icon_stylesheet() {
-	wp_register_style( 'fontawesome', 'https:////maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
-	wp_enqueue_style( 'fontawesome');
+	// Import Bulma Framework
+	wp_register_style( 'bulma-framework', get_template_directory_uri() . '>/vendor/bulma-0.9.0/css/bulma.min.css' );
+	wp_enqueue_style( 'bulma-framework' );
+
+	// Import Bulma Carousel
+	wp_register_style( 'bulma-carousel', get_template_directory_uri() . '/vendor/bulma-carousel-4.0.4/bulma-carousel.min.css' );
+	wp_enqueue_style( 'bulma-carousel' );
+
+	// Import Cookie Script Stylesheets
+	wp_register_style( 'cookie-style', get_template_directory_uri() . '/vendor/dywc_1.1/dywc.css' );
+	wp_enqueue_style( 'cookie-style' );
+
+	//  Import Theme Styles
+	wp_register_style( 'style', get_stylesheet_uri() );
+	wp_enqueue_style( 'style' );
+
 }
 
-add_action( 'wp_enqueue_scripts', 'autoglasdienst_enqueue_icon_stylesheet' );
+add_action( 'wp_enqueue_scripts', 'autoglasdienst_register_styles' );
 
 
-	 
+function autoglasdienst_register_scripts() {
+
+	// Import JQery 1.4.3
+	wp_register_script( 'jquery-1-4-3', get_template_directory_uri() . '/vendor/jquery-1.4.3/jquery.min.js', '', null, true );
+	wp_enqueue_script( 'jquery-1-4-3' );
+
+	// Import Bulma Extensions
+	wp_register_script( 'bulma-extensions', get_template_directory_uri() . '/vendor/bulma-carousel-4.0.4/bulma-carousel.min.js', '', null, true );
+	wp_enqueue_script( 'bulma-extensions' );
+
+	// Import Theme Scripts
+	wp_register_script( 'theme-scripts', get_template_directory_uri() . '/assets/js/carousel-slider.js', '', null, true );
+	wp_enqueue_script( 'theme-scripts' );
+
+	// Button Back-to-Top
+	wp_register_script( 'button-back-to-top', get_template_directory_uri() . '/assets/js/button-back-to-top.js', '', null, true );
+	wp_enqueue_script( 'button-back-to-top' );
+
+
+	// Import Cookie Notice Scripts
+	wp_register_script( 'dywc', get_template_directory_uri() . '/assets/scripts/dywc.js', '', null, true );
+	wp_enqueue_script( 'dywc' );
+
+	wp_register_script( 'cookie-notice', get_template_directory_uri() . '/assets/scripts/cookie-notice.js', '', null, true );
+	wp_enqueue_script( 'cookie-notice' );
+
+
+}
+
+add_action( 'wp_enqueue_scripts', 'autoglasdienst_register_scripts' );
+
+
+
+
+
 /* Menü Support */
 
 function autoglasdienst_register_menu() {
@@ -60,18 +123,18 @@ require_once('classes/bulma-navwalker.php');
 /* Navigation Walker für Footermenü */
 
 class Footer_Walker extends Walker_Nav_Menu {
-	
+
 	function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
-	
+
 	$classes = empty($item->classes) ? array () : (array) $item->classes;
-	
+
 	$class_names = join(' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
-		
+
 		!empty ( $class_names ) and $class_names = ' class="'. esc_attr( $class_names ) . '"';
-		
+
 		$output .= "<li>";
 		$attributes  = 'class="footer-menu__link"';
-		
+
 		!empty( $item->attr_title ) and $attributes .= ' title="'  . esc_attr( $item->attr_title ) .'"';
 		!empty( $item->target ) and $attributes .= ' target="' . esc_attr( $item->target     ) .'"';
 		!empty( $item->xfn ) and $attributes .= ' rel="'    . esc_attr( $item->xfn        ) .'"';
@@ -99,30 +162,17 @@ include('classes/custom-shortcodes.php');
 
 
 
-	
-	
-/* Beitragsformate */
-	
-add_theme_support( 'post-formats', array( 'gallery' ) );
-	
-
-
-/* Beitragsbilder */
-	
-add_theme_support( 'post-thumbnails' ); 
-
-
 
 /* Support des Dateityps SVG */
 
 function autoglasdienst_add_upload_ext($checked, $file, $filename, $mimes){
- 
+
 	if(!$checked['type']){
 		$wp_filetype = wp_check_filetype( $filename, $mimes );
 		$ext = $wp_filetype['ext'];
 		$type = $wp_filetype['type'];
 		$proper_filename = $filename;
-	 
+
 		if($type && 0 === strpos($type, 'image/') && $ext !== 'svg'){
 			$ext = $type = false;
 		}
@@ -131,13 +181,13 @@ function autoglasdienst_add_upload_ext($checked, $file, $filename, $mimes){
 	return $checked;
 }
 add_filter('wp_check_filetype_and_ext', 'autoglasdienst_add_upload_ext', 10, 4);
-	
+
 
 
 
 
 /* Fügt das Slug-Adds the Slug to the body tag's class  */
- 
+
 function add_slug_body_class( $classes ) {
 	 global $post;
   if ( isset( $post ) ) {
@@ -202,17 +252,17 @@ add_action('manage_posts_custom_column', 'autoglasdienst_preview_thumb_column', 
 /* Erzeugung einer Breadcrumb-Navigation */
 
 function nav_breadcrumb() {
- 
+
 	// $delimiter = '&raquo;';
 	$delimiter = '';
-	$before = '<li class="is-active">'; 
-	$after = '</li>'; 
-		 
+	$before = '<li class="is-active">';
+	$after = '</li>';
+
 	if ( !is_home() && !is_front_page() || is_paged() ) {
-	 	
+
 		global $post;
 		$homeLink = get_bloginfo('url');
-		
+
 		echo '<nav class="breadcrumb has-succeeds-separator" aria-label="breadcrumbs">';
 		echo '<ul>';
 		echo '<li>';
@@ -223,105 +273,105 @@ function nav_breadcrumb() {
 			echo '</a>';
 		echo ' </li>';
 		echo $delimiter;
-	 
+
 		if ( is_category()) {
-			
+
 			global $wp_query;
 			$cat_obj = $wp_query->get_queried_object();
 			$thisCat = $cat_obj->term_id;
 			$thisCat = get_category($thisCat);
 			$parentCat = get_category($thisCat->parent);
-		 
+
 			if ($thisCat->parent != 0) echo(get_category_parents($parentCat, TRUE, ' ' . $delimiter . ' '));
 			echo $before . single_cat_title('', false) . $after;
-		 
+
 		} elseif ( is_day() ) {
-			
+
 			echo '<li><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a></li> ' . $delimiter . ' ';
 			echo '<li><a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a></li> ' . $delimiter . ' ';
 			echo $before . get_the_time('d') . $after;
-		 
+
 		} elseif ( is_month() ) {
-		
+
 			echo '<li><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a></li> ' . $delimiter . ' ';
 			echo $before . get_the_time('F') . $after;
-		 
+
 		} elseif ( is_year() ) {
-			
+
 			echo $before . get_the_time('Y') . $after;
-		 
+
 		} elseif ( is_single() && !is_attachment() ) {
-		
+
 			if ( get_post_type() != 'post' ) {
 			$post_type = get_post_type_object(get_post_type());
 			$slug = $post_type->rewrite;
-			
+
 			echo '<li><a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->singular_name . '</a></li> ' . $delimiter . ' ';
 			echo $before . get_the_title() . $after;
-		
+
 			} else {
-		
+
 				$cat = get_the_category(); $cat = $cat[0];
 				echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
 				echo $before . get_the_title() . $after;
-			
+
 			}
-		 
+
 		 } elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
-		 
+
 		 $post_type = get_post_type_object(get_post_type());
 		 echo $before . $post_type->labels->singular_name . $after;
-		 
+
 		} elseif ( is_attachment() ) {
-			
+
 			$parent = get_post($post->post_parent);
 			$cat = get_the_category($parent->ID); $cat = $cat[0];
-			
+
 			echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
 			echo '<li><a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a></li> ' . $delimiter . ' ';
 			echo $before . get_the_title() . $after;
-		 
+
 		} elseif ( is_page() && !$post->post_parent ) {
-			
+
 			echo $before . get_the_title() . $after;
-		 
+
 		} elseif ( is_page() && $post->post_parent ) {
-		
+
 			$parent_id = $post->post_parent;
 			$breadcrumbs = array();
-		
+
 			while ($parent_id) {
-				
+
 				$page = get_page($parent_id);
 				$breadcrumbs[] = '<li><a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a></li> ';
 				$parent_id = $page->post_parent;
 			}
-		 
+
 			$breadcrumbs = array_reverse($breadcrumbs);
-			
+
 			foreach ($breadcrumbs as $crumb) echo $crumb . ' ' . $delimiter . ' ';
 			echo $before . get_the_title() . $after;
-		 
+
 		} elseif ( is_search() ) {
 			echo $before . 'Ergebnisse für Ihre Suche nach "' . get_search_query() . '"' . $after;
-		 
+
 		} elseif ( is_tag() ) {
 			echo $before . 'Beiträge mit dem Schlagwort "' . single_tag_title('', false) . '"' . $after;
-		
+
 		} elseif ( is_404() ) {
 			echo $before . 'Fehler 404' . $after;
 		}
-		 
+
 		if ( get_query_var('paged') ) {
 			if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
 			echo ': ' . __('Seite') . ' ' . get_query_var('paged');
 			if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
 		}
-	 
+
 		echo '</nav>';
-	 
-	} 
-} 
+
+	}
+}
 
 
 
@@ -334,20 +384,20 @@ function nav_breadcrumb() {
 
 
 
-/* --- CUSTOM LOGIN BEREICH --- */  
- 
-/* Eigenes Styling des Login-Bereichs */ 
+/* --- CUSTOM LOGIN BEREICH --- */
+
+/* Eigenes Styling des Login-Bereichs */
 
 function autoglasdienst_custom_login_stylesheet() {
 	wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/templates/login/css/style-login.css' );
 	wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/templates/login/css/style-login.js' );
 }
 
-add_action( 'login_enqueue_scripts', 'autoglasdienst_custom_login_stylesheet' ); 
- 
- 
-/* Funktion "Remember-me" standardmäßig aktivieren */  
- 
+add_action( 'login_enqueue_scripts', 'autoglasdienst_custom_login_stylesheet' );
+
+
+/* Funktion "Remember-me" standardmäßig aktivieren */
+
 function autoglasdienst_login_checked_remember_me() {
 add_filter( 'login_footer', 'rememberme_checked' );
 }
@@ -357,26 +407,26 @@ add_action( 'init', 'autoglasdienst_login_checked_remember_me' );
 
 function rememberme_checked() {
 echo "<script>document.getElementById('rememberme').checked = true;</script>";
-} 
- 
-	
-/* Standardmäßige Fehlermeldung beim Anmelden überschreiben */ 
+}
+
+
+/* Standardmäßige Fehlermeldung beim Anmelden überschreiben */
 
 function autoglasdienst_login_error_override()
 {
 	return 'Die Zugangsdaten sind leider nicht korrekt.';
 }
 
-add_filter('login_errors', 'autoglasdienst_login_error_override');	
+add_filter('login_errors', 'autoglasdienst_login_error_override');
 
 
 
-/* Umleitung der standardmäßigen Login-Seite auf die Custom Login Page */ 
+/* Umleitung der standardmäßigen Login-Seite auf die Custom Login Page */
 
 function autoglasdienst_redirect_login_page() {
   $login_page  = home_url( '/login/' );
   $page_viewed = basename($_SERVER['REQUEST_URI']);
- 
+
   if( $page_viewed == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
 	wp_redirect($login_page);
 	exit;
@@ -392,7 +442,7 @@ add_action('init','autoglasdienst_redirect_login_page');
 
 
 /* Weiterleitung nach erfolgreichem Login */
- 
+
 function autoglasdienst_login_redirect( $redirect_to, $request, $user ) {
 	//is there a user to check?
 	if ( isset( $user->roles ) && is_array( $user->roles ) ) {
@@ -407,14 +457,14 @@ function autoglasdienst_login_redirect( $redirect_to, $request, $user ) {
 		return $redirect_to;
 	}
 }
- 
+
 add_filter( 'login_redirect', 'autoglasdienst_login_redirect', 10, 3 );
 
 
 
 
 
-/* Abfangen der Login-Fehlversuche */ 
+/* Abfangen der Login-Fehlversuche */
 
 function autoglasdienst_login_failed() {
   $login_page  = home_url( '/login/' );
@@ -423,8 +473,8 @@ function autoglasdienst_login_failed() {
 }
 
 add_action( 'wp_login_failed', 'autoglasdienst_login_failed' );
- 
- 
+
+
 function autoglasdienst_verify_username_password( $user, $username, $password ) {
   $login_page  = home_url( '/login/' );
 	if( $username == "" || $password == "" ) {
@@ -437,7 +487,7 @@ add_filter( 'authenticate', 'autoglasdienst_verify_username_password', 1, 3);
 
 
 
-/* Abfangen der Umleitung auf die Login-Seite nach dem Ausloggen */ 
+/* Abfangen der Umleitung auf die Login-Seite nach dem Ausloggen */
 
 function autoglasdienst_logout_page() {
   $login_page  = home_url( '/login/' );
